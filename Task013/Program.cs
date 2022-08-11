b7e217b67d7273e0bbcba2c77d2a7b0b0f53d750
Petukhov-work <petukhov-work@yandex.ru>
2022-08-11 12:52:18 +0300

    второе ДЗ

---
+++ Task013/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число: ");
+int a= Convert.ToInt32(Console.ReadLine()) ;
+
+int b=(a/10/10)%10;
+if (b>0) Console.WriteLine(b);
+else {Console.WriteLine("третьей цифры нет");}
---
+++ Task015/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число от 1 до 7: ");
+int a= Convert.ToInt32(Console.ReadLine()) ;
+
+if (a==6 || a==7) {Console.WriteLine("Да");}
+else {Console.WriteLine("Нет");}
