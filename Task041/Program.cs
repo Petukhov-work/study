792037d58fdc8009d3c4f56af8a76ed093b53e6b
Petukhov-work <petukhov-work@yandex.ru>
2022-08-26 17:03:13 +0300

    6 дз

---
+++ Task041/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int [] FillingArray (int length)
+{
+    int [] array=new int [length];
+
+    for (int i=0; i<length; i++)
+    {
+       Console.WriteLine("Введите число в массив : ");
+       array[i] = Convert.ToInt32(Console.ReadLine()) ;
+    }
+    return array;
+}
+
+
+
+int PositiveNumber (int [] array)
+{
+    int count=0;
+    for (int i=0; i<array.Length;i=i+1)
+    {
+        if (array[i]>0 ) count++;
+    }
+    return count;
+}
+
+
+Console.WriteLine("Введите количество чисел в массиве : ");
+int lengthArray= Convert.ToInt32(Console.ReadLine()) ;
+
+int [] array=FillingArray(lengthArray);
+
+
+Console.WriteLine("Количество положительных элементов в массиве: ");
+Console.Write(PositiveNumber(array));
---
+++ Task043/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int IntersectionPointX (int k1, int b1, int k2, int b2) 
+{
+    return  (b2-b1)/(k1-k2);
+}
+
+Console.WriteLine("Введите значения k1: ");
+int k1= Convert.ToInt32(Console.ReadLine()) ;
+
+Console.WriteLine("Введите значения b1: ");
+int b1= Convert.ToInt32(Console.ReadLine()) ;
+
+Console.WriteLine("Введите значения k2: ");
+int k2= Convert.ToInt32(Console.ReadLine()) ;
+
+Console.WriteLine("Введите значения b2: ");
+int b2= Convert.ToInt32(Console.ReadLine()) ;
+
+
+int pointX=IntersectionPointX(k1,b1,k2,b2);
+int pointY=k1*pointX+b1;
+
+Console.WriteLine($"Точка перечений прямых: x={pointX}, y={pointY}");
