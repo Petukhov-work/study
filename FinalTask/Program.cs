7e61a50bbf854a88d9ff6146009a29bb89657990
Petukhov-work <petukhov-work@yandex.ru>
2022-09-16 19:41:45 +0300

    создание массива

---
+++ FinalTask/Program.cs
@@ -0,0 +1,11 @@
+﻿str [] MassivStr (int length)
+{
+    str [] array = new str [length];
+    for (int i=0; i<array.Length;i=i+1)
+    {
+        Console.WriteLine("Введите слово в массив");
+        array[i]=Console.ReadLine();
+    } 
+    return array;
+}
+
